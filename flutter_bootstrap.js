4a43e7e9b4a694ba720e89556b2a9d729c3bef63
jyunjung <jyunjung@gmail.com>
2025-07-27 11:32:09 +0900

    Deploy from budgetapp: 2025-07-27 11:32:08

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"dd93de6fb1776398bf586cbd477deade1391c7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "427181089"
+    serviceWorkerVersion: "1218989888"
   }
 });
