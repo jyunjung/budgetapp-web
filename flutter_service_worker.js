f1ea78cb316438d4a7b704f82d468f6b57ae6d34
jyunjung <jyunjung@gmail.com>
2025-10-03 19:39:57 +0900

    Deploy from budgetapp: 2025-10-03 19:39:57

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c70cf5c810235b7622a7b2faecf09709
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "7b7da4d557f69a531c09f9a9b58fc8bd",
+"flutter_bootstrap.js": "7982e30270f9f6fe88dac4c6b70fa2b7",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
